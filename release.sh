61e28073ecf64cec0fbef590bdee72134e0a7f4d
Tomas Bjerre <tomas.bjerre85@gmail.com>
2025-07-28 11:13:07 +0200

    feat: triggering minor version

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-gpg -o $(date +%s%3N) --sign .gitignore \
+gpg -o /tmp/$(date +%s%3N) --sign .gitignore \
  && ./mvnw se.bjurr.gitchangelog:git-changelog-maven-plugin:semantic-version \
-  && (git commit -a -m "chore: setting version in pom" && git push || echo "No new version") #\
-#  && ./mvnw release:prepare release:perform -B \
-#  && ./mvnw se.bjurr.gitchangelog:git-changelog-maven-plugin:git-changelog \
-#  && git commit -a -m "chore: updating changelog" \
-#  && git push \
-#  || git clean -f
+  && (git commit -a -m "chore: setting version in pom" && git push || echo "No new version") \
+  && ./mvnw release:prepare release:perform -B \
+  && ./mvnw se.bjurr.gitchangelog:git-changelog-maven-plugin:git-changelog \
+  && git commit -a -m "chore: updating changelog" \
+  && git push \
+  || git clean -f
