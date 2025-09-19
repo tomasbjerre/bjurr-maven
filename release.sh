fab674fb7937e18ca4576eedd13e9e209bd51103
Tomas Bjerre <tomas.bjerre85@gmail.com>
2025-09-19 19:33:43 +0200

    chore: setting version in pom

--- release.sh
+++ release.sh
@@ -3,8 +3,4 @@
 gpg -o /tmp/$(date +%s%3N) --sign .gitignore \
  && ./mvnw se.bjurr.gitchangelog:git-changelog-maven-plugin:semantic-version \
   && (git commit -a -m "chore: setting version in pom" && git push || echo "No new version") \
-  && ./mvnw release:prepare release:perform -B \
-  && ./mvnw se.bjurr.gitchangelog:git-changelog-maven-plugin:git-changelog \
-  && git commit -a -m "chore: updating changelog" \
-  && git push \
-  || git clean -f
+  && ./mvnw release:prepare 
