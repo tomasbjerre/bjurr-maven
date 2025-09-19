353ca265b02fec944fe321d598c9123b07084db3
Tomas Bjerre <tomas.bjerre85@gmail.com>
2025-09-19 19:41:26 +0200

    fix: removing git-changelog-maven-plugin from pom to avoid snapshot to be set during release

--- release.sh
+++ release.sh
@@ -3,4 +3,8 @@
 gpg -o /tmp/$(date +%s%3N) --sign .gitignore \
  && ./mvnw se.bjurr.gitchangelog:git-changelog-maven-plugin:semantic-version \
   && (git commit -a -m "chore: setting version in pom" && git push || echo "No new version") \
-  && ./mvnw release:prepare 
+  && ./mvnw release:prepare release:perform -B \
+  && ./mvnw se.bjurr.gitchangelog:git-changelog-maven-plugin:git-changelog \
+  && git commit -a -m "chore: updating changelog" \
+  && git push \
+  || git clean -f
